489049d7ac5323f240074d679762c5162333421d
ktrianaa2 <ktrianaa2@uteq.edu.ec>
2024-02-09 11:27:01 -0500

    ajustes cliente

--- app/src/main/java/com/example/apphamburguesas/Cliente.java
+++ app/src/main/java/com/example/apphamburguesas/Cliente.java
@@ -3,12 +3,24 @@ package com.example.apphamburguesas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Cliente extends AppCompatActivity {
+
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
+
+        bottomNav = findViewById(R.id.bottomNav);
+
+        bottomNav.setOnNavigationItemSelectedListener(item -> {
+            Toast.makeText(Cliente.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            return true;
+        });
     }
 }
