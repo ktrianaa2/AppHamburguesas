24dea08adba44b5ae1bbb3eeef75c0e657c7e36d
ktrianaa2 <ktrianaa2@uteq.edu.ec>
2024-03-08 17:13:32 -0500

    vista modulos administrador

--- app/src/main/java/com/example/apphamburguesas/adm/admEmpresa.java
+++ app/src/main/java/com/example/apphamburguesas/adm/admEmpresa.java
@@ -3,6 +3,8 @@ package com.example.apphamburguesas.adm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.apphamburguesas.R;
 
@@ -12,5 +14,16 @@ public class admEmpresa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adm_empresa);
+
+        ImageView imageViewFlecha = findViewById(R.id.imageView2);
+
+        imageViewFlecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cierra la actividad actual y regresa a la anterior
+                finish();
+            }
+        });
+
     }
 }
